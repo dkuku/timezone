a03c0ce80632c0a1fba5fae673763fef58a69476
Daniel Kukula <daniel.kuku@gmail.com>
2018-10-25 02:16:15 +0100

    GET route

--- app/api.rb
+++ app/api.rb
@@ -5,11 +5,23 @@ require 'nearest_time_zone'
 module Timezone
   class API < Sinatra::Base
     post '/timezone' do
+      begin
       request.body.rewind
       data = JSON.parse request.body.read
-      lat = data['latitude']
-      lng = data['longitude']
+      lat = data['latitude'].to_i
+      lng = data['longitude'].to_i
+        timezone = NearestTimeZone.to(lat, lng)
+        JSON timezone: timezone
+      rescue => error
+        status 422
+        JSON error: error.message
+      end
+    end
+
+    get '/timezone/:lat/:lng' do
       begin
+        lat = params[:lat].to_i
+        lng = params[:lng].to_i
         timezone = NearestTimeZone.to(lat, lng)
         JSON timezone: timezone
       rescue => error
--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -40,5 +40,32 @@ module Timezone
         end
       end
     end
+    describe 'GET /timezone' do
+      context 'when the position is correct' do
+        before do
+          get "/timezone/#{poland[:latitude]}/#{poland[:longitude]}"
+        end
+        it 'returns with the timezone for position' do
+        parsed = JSON.parse(last_response.body)
+        expect(parsed).to include('timezone' => 'Europe/Warsaw')
+        end
+
+        it 'responds with 200 (OK)' do
+          expect(last_response.status).to eq(200)
+        end
+      end
+      context 'when the position is wrong or missing' do
+        before do
+          get "/timezone/#{wrong[:latitude]}/#{wrong[:longitude]}"
+        end
+        it 'returns an error message' do
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('error' => be_kind_of(String))
+        end
+        it 'responds with 422 (Unprocessable entity)' do
+          expect(last_response.status).to eq(422)
+        end
+      end
+    end
   end
 end
