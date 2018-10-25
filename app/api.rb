60d2c2eaf020d9abb844980e20548e87250c0568
Daniel Kukula <daniel.kuku@gmail.com>
2018-10-25 02:34:09 +0100

    added basic instructions

--- app/api.rb
+++ app/api.rb
@@ -6,10 +6,10 @@ module Timezone
   class API < Sinatra::Base
     post '/timezone' do
       begin
-      request.body.rewind
-      data = JSON.parse request.body.read
-      lat = data['latitude'].to_i
-      lng = data['longitude'].to_i
+        request.body.rewind
+        data = JSON.parse request.body.read
+        lat = data['latitude'].to_i
+        lng = data['longitude'].to_i
         timezone = NearestTimeZone.to(lat, lng)
         JSON timezone: timezone
       rescue => error
@@ -27,6 +27,23 @@ module Timezone
       rescue => error
         status 422
         JSON error: error.message
+      end
+
+      get "/" do
+        %q{
+     <html>
+       <head></head>
+       <body>
+         <div>
+          <h1>Timezone api</h1>
+           <p>Usage:</p>
+           <p>send a get request to /timezone/:latitude/:longitude</p>
+           <p>It is possible also to send a post request - please check the
+            <a href="https://github.com/dkuku/timezone">source code</a>on github</p>
+         </div>
+       </body>
+     </html>
+     }
       end
     end
   end
