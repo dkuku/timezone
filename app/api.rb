24431c1251cdf98e33c141755193fe2a2af076c0
Daniel Kukula <daniel.kuku@gmail.com>
2018-10-25 02:47:35 +0100

    external index file

--- app/api.rb
+++ app/api.rb
@@ -28,23 +28,10 @@ module Timezone
         status 422
         JSON error: error.message
       end
+    end
 
-      get "/" do
-        %q{
-     <html>
-       <head></head>
-       <body>
-         <div>
-          <h1>Timezone api</h1>
-           <p>Usage:</p>
-           <p>send a get request to /timezone/:latitude/:longitude</p>
-           <p>It is possible also to send a post request - please check the
-            <a href="https://github.com/dkuku/timezone">source code</a>on github</p>
-         </div>
-       </body>
-     </html>
-     }
-      end
+    get '/' do
+      File.read(File.join('public', 'index.html'))
     end
   end
 end
